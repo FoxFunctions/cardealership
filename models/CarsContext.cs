1a80d589daead1f07d5db687fb609b0d54f24e42
FoxFunctions <95949981+FoxFunctions@users.noreply.github.com>
2022-05-23 10:11:11 -0400

    Update CarsContext.cs

--- models/CarsContext.cs
+++ models/CarsContext.cs
@@ -23,7 +23,7 @@ namespace CarDealership.models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost; Database=Cars;User=SA; Password=skN9fKL890!;");
+                optionsBuilder.UseSqlServer("Server=localhost; Database=Cars;User=SA; Password={Password}!;");
             }
         }
 
